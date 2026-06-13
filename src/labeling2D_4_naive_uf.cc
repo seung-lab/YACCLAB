195746b70ed86ec79418c475a80b0ec5c44db361
William Silversmith <william.silversmith@gmail.com>
2026-06-13 00:19:50 -0400

    wip: trying to add a naive 4-connected algorithm

---
+++ src/labeling2D_4_naive_uf.cc
@@ -0,0 +1,9 @@
+// Copyright (c) 2026, the YACCLAB contributors, as 
+// shown by the AUTHORS file. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "labeling2D_4_naive_uf.h"
+
+REGISTER_LABELING_WITH_EQUIVALENCES_SOLVERS(naive_2d_4_uf);
