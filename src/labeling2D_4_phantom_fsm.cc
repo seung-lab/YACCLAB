82e0b73dbae49f0a1a80a0575a656191d5cbaba4
William Silversmith <william.silversmith@gmail.com>
2026-06-17 21:09:52 -0400

    feat: add phantom fsm

---
+++ src/labeling2D_4_phantom_fsm.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2026, the YACCLAB contributors, as 
+// shown by the AUTHORS file. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "labeling2D_4_phantom_fsm.h"
+#include "labels_solver.h"
+
+REGISTER_LABELING_WITH_EQUIVALENCES_SOLVERS(PHANTOM_FSM_2D_4);
