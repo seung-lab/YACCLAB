49418ca94af05ca6f92df3bbb3b35972931a3fd6
William Silversmith <william.silversmith@gmail.com>
2026-06-19 18:46:55 -0400

    feat: add 6c sauf implementation

--- include/labeling_algorithms.h
+++ include/labeling_algorithms.h
@@ -106,7 +106,7 @@ public:
 
     virtual ~Labeling3D() = default;
 
-    virtual std::string CheckAlg() const { return LabelingCheckSingleton3D::GetCheckAlg(Conn, LabelBackground); }
+    virtual std::string CheckAlg() const override { return LabelingCheckSingleton3D::GetCheckAlg(Conn, LabelBackground); }
 
     virtual bool IsLabelBackground() const override { return LabelBackground; }
 
---
+++ src/labeling3D_sauf_6c.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2020, the YACCLAB contributors, as 
+// shown by the AUTHORS file. All rights reserved.
+//
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+#include "labeling3D_sauf_6c.h"
+#include "labels_solver.h"
+
+REGISTER_LABELING_WITH_EQUIVALENCES_SOLVERS(SAUF6C)
