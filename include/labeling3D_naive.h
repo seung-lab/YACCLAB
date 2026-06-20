9caf6b4f2c5f3e58d9389188604937b559855dd3
William Silversmith <william.silversmith@gmail.com>
2026-06-20 01:47:55 -0400

    perf: faster labeling3d_naive.h

--- include/labeling3D_naive.h
+++ include/labeling3D_naive.h
@@ -124,14 +124,10 @@ public:
 		// Second scan
 		LabelsSolver::Flatten();
 
+		const int voxels = img_labels_.size[0] * img_labels_.size[1] * img_labels_.size[2];
 		int * img_row = reinterpret_cast<int*>(img_labels_.data);
-		for (int z = 0; z < img_labels_.size[0]; z++) {
-			for (int y = 0; y < img_labels_.size[1]; y++) {
-				for (int x = 0; x < img_labels_.size[2]; x++) {
-					img_row[x] = LabelsSolver::GetLabel(img_row[x]);
-				}
-				img_row += img_labels_.step[1] / sizeof(int);
-			}
+		for (int i = 0; i < voxels; i++) {
+			img_row[i] = LabelsSolver::GetLabel(img_row[i]);
 		}
 
 		LabelsSolver::Dealloc(); // Memory deallocation of the labels solver
@@ -282,14 +278,10 @@ public:
 		// Second scan
 		LabelsSolver::Flatten();
 
+		const int voxels = img_labels_.size[0] * img_labels_.size[1] * img_labels_.size[2];
 		int * img_row = reinterpret_cast<int*>(img_labels_.data);
-		for (int z = 0; z < img_labels_.size[0]; z++) {
-			for (int y = 0; y < img_labels_.size[1]; y++) {
-				for (int x = 0; x < img_labels_.size[2]; x++) {
-					img_row[x] = LabelsSolver::GetLabel(img_row[x]);
-				}
-				img_row += img_labels_.step[1] / sizeof(int);
-			}
+		for (int i = 0; i < voxels; i++) {
+			img_row[i] = LabelsSolver::GetLabel(img_row[i]);
 		}
 	}
 };
